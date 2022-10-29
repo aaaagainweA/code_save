23b84e308e5e48d7800ff0bba1f72301e47fbad6
aaaagainweA <116945457+aaaagainweA@users.noreply.github.com>
2022-10-29 16:54:45 +0800

    Add files via upload

---
+++ sales_summary/lbkf_yesterday.py
@@ -0,0 +1,147 @@
+﻿# Python文件
+# 三方库
+import pandas as pd
+import pymongo
+import pymysql
+import datetime
+import time
+from bson.objectid import ObjectId
+import warnings
+warnings.filterwarnings('ignore')
+# 连接mongdb
+conn = pymongo.MongoClient('mongodb://{}:{}@{}:{}/?authSource={}'
+                       .format("cda_epmall", "3PUkMsWCTu424Renrffb",
+                               "dds-wz99d33fe2f72610a042-pub.mongodb.rds.aliyuncs.com",
+                               "3717", "epmall"))
+conn_epmall = conn["epmall"]
+
+
+#筛选当月的数据
+def object_id_from_datetime(from_datetime=None,span_days=0,span_hours=0,span_minutes=0,span_seconds=0,span_weeks=0):
+    '''根据时间手动生成一个objectid，此id不作为存储使用'''
+    if not from_datetime:
+        from_datetime = datetime.datetime.now()
+    from_datetime = from_datetime + datetime.timedelta(days=span_days,hours=span_hours,minutes=span_minutes,weeks=span_weeks)
+    return ObjectId.from_datetime(generation_time=from_datetime)
+x = datetime.date.today()
+oneday = datetime.timedelta(days=1)
+y = x - oneday
+str_month=datetime.datetime(y.year, y.month, 1)
+str_date=str(str_month)[:10]
+en_date = str(y)
+date=str(x)
+print(str_date,en_date)
+begin_date = str(time.mktime(time.strptime(str_date, "%Y-%m-%d")))
+end_date = str(time.mktime(time.strptime(date, "%Y-%m-%d")))
+date_list=[]
+while True:
+    date_list.append(str(x))
+    if str(x) == str(str_month)[:10]:
+        break
+    x = x-oneday
+
+
+col_order = conn_epmall['epmall_orders_100063']
+df_order = pd.DataFrame(list(col_order.find({'create_date':{'$gt':begin_date,'$lt':end_date},'state':{'$gte':1,'$lte':9}},
+                                             {'_id':0,'state':1,'order_no':1,'create_date':1,'user_id':1,'nick':1,'mobile':1,
+                                              'isback':1,'commodity_id':1,'commodity_name':1,'accounted_money':1,'remark_cus':1,
+                                              'recommend_store_name':1})))
+
+col_make=conn_epmall['epmall_appointment_100063'] #找出预约门店
+df_make=pd.DataFrame(list(col_make.find({},{'_id':0,'store_name':1,'phone':1})))
+
+
+col_store=conn_epmall['epmall_store_100063'] #找出预约城市
+df_store=pd.DataFrame(list(col_store.find({},{'_id':0,'store_name':1,'city':1})))
+
+df_new=pd.merge(df_make,df_store,on='store_name') #以为store_name基准合并两表
+
+col_put=conn_epmall['ccc_user']
+df_put=pd.DataFrame(list(col_put.find({},{'_id':0,'phone':1,'province':1,'city':1})))
+df_put=df_put.rename(columns={'city':'投流城市'})
+
+col_app_user=conn_epmall['epmall_app_user_100063'] #小程序用户表
+col_app_user=pd.DataFrame(list(col_app_user.find({},{'_id':0,'phone':1,'deliver_name':1})))
+
+df_new3=pd.merge(df_put,df_new,on='phone',how='outer')
+
+df_new4=pd.merge(df_new3,col_app_user,on='phone',how='outer')
+df_new4.rename(columns={'store_name':'预约门店','city':'预约城市','phone':'电话','province':'投流省份','deliver_name':'客户渠道'},inplace=True)
+
+
+
+# 将时间戳转化为时间格式
+def time_t(x):
+    if x=='':
+        return ''
+    elif x==' ':
+        return ''
+    elif str(x)=='nan':
+        return ''
+    elif str(x)=='NaN':
+        return ''
+    else:
+        x=int(x)
+        time_local = time.localtime(x/1000)
+        create_date = time.strftime('%Y-%m-%d %H:%M:%S', time_local)
+        return create_date
+df1_order = df_order.loc[:,['order_no','create_date','user_id','nick','mobile','isback','commodity_id','commodity_name','accounted_money','remark_cus','recommend_store_name']]
+df1_order['accounted_money'] = df1_order['accounted_money'] / 100
+df1_order['create_date'] = df1_order['create_date'].apply(lambda x: time_t(x))
+df1_order['create_time'] = df1_order['create_date'].apply(lambda x: str(x)[11:])
+df1_order['create_date'] = df1_order['create_date'].apply(lambda x: str(x)[:10])
+
+col_commodity=conn_epmall['epmall_commodity_100063'] # 商品表
+df_commodity=pd.DataFrame(list(col_commodity.find({},
+                                             {'_id':0,'commodity_id':1,
+                                              'variety_id':1,'variety_name':1})))
+
+variety_id=['1646038407932','1646209380573','1646491684554']
+df_commodity = df_commodity[df_commodity['variety_id'].isin(variety_id)]
+commodity_id_list=list(df_commodity['commodity_id'])
+df2_order = df1_order[df1_order['commodity_id'].isin(commodity_id_list)]
+df2_order=pd.merge(df2_order,df_commodity,on='commodity_id',how='left')
+
+db=pymysql.connect(host='rm-wz9220210vr0pila6guy90110-pub.mysql.rds.aliyuncs.com',database='epmall',user='cda_epmall',password='3PUkMsWCTu424Renrffb',port=3306,charset='utf8')
+sql='select * from user'
+df_user=pd.read_sql(sql,db)
+user1=df_user.loc[:,['username','dept_id','id']]
+user1=user1.rename(columns={'id':'u_id'})
+user1.loc[user1['dept_id'].isnull(),'dept_id']=0
+user1['dept_id']=user1['dept_id'].apply(lambda x:int(x))
+sql1='select * from dept'
+df_dept=pd.read_sql_query(sql1,db)
+dept=df_dept.loc[:,['id','name']]
+user_dept=pd.merge(user1,dept,left_on='dept_id',right_on='id',how='left')
+user_dept.fillna('0',inplace=True)
+
+ccc_share=conn_epmall["epmall_share_order_detail_100063"]
+df_share=pd.DataFrame(list(ccc_share.find()))
+share_1=df_share.loc[(df_share['delete_flag']==0),['share_id','order_no','belong_user_name','belong_user_id']]
+df1_user_dept = pd.merge(user_dept,share_1,left_on='u_id', right_on='belong_user_id',how='right')
+
+df3_order = pd.merge(df2_order,df1_user_dept,on='order_no',how='left')
+
+df4_order = df3_order.loc[:,
+            ['order_no', 'user_id', 'nick', 'mobile', 'create_date', 'create_time', 'accounted_money', 'isback',
+             'commodity_name','variety_name', 'remark_cus', 'recommend_store_name', 'belong_user_name', 'name']]
+df4_order['isback'] = df4_order['isback'].apply(lambda x: '否' if  x==0 else '是')
+df4_order['remark_cus']=df4_order['remark_cus'].apply(lambda x:str(x)[:10])
+df4_order.columns = ['订单编号','用户ID','昵称','电话','订单创建日期','订单创建时间','实付金额','是否退款','套餐名称','套餐分类','订单来源','门店','营销客服','部门']
+ccc_1=conn_epmall["ccc_user"]
+ccc_2=pd.DataFrame(list(ccc_1.find({},{'_id':0,'phone':1,'create_time':1,'delete_flag':1})))
+ep_ccc=ccc_2.loc[(ccc_2['delete_flag']==0),['phone','create_time']]
+ep_ccc['create_time']=ep_ccc['create_time'].apply(lambda x:time_t(x))
+ep_ccc2=ep_ccc.groupby('phone')['create_time'].min().reset_index()
+result=pd.merge(df4_order,ep_ccc2,left_on='电话',right_on='phone',how='left')
+result.rename(columns={'create_time':'首次导粉日期'},inplace=True)
+
+
+result=pd.merge(result,df_new4,on='电话',how='left')
+result['首次导粉时间'] = pd.to_datetime(result['首次导粉日期'],format='%Y-%m-%d %H:%M:%S').dt.time
+result['首次导粉日期'] = pd.to_datetime(result['首次导粉日期'],format='%Y-%m-%d %H:%M:%S').dt.date
+
+result_last=result.loc[:,['订单编号','用户ID','昵称','电话','订单创建日期','订单创建时间','实付金额','是否退款','套餐名称','套餐分类','订单来源','门店','首次导粉日期','营销客服','部门','预约门店','预约城市','投流省份','投流城市','客户渠道']]
+result_last.drop_duplicates(subset='订单编号',keep='last',inplace=True) #去重
+result_last.to_excel( './礼宾客服.xlsx', index=False, header=True)
+conn.close()
---
+++ sales_summary/sales_collect_day.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import excel2img
+import datetime
+import requests
+import base64
+import hashlib
+import os
+from copy import copy
+from urllib3 import encode_multipart_formdata
+
+today = datetime.date.today()
+oneday = datetime.timedelta(days=1)
+yesterday = today - oneday
+date=str(yesterday)
+date1=str(today)
+
+#-----------销售日报计算-----------
+
+df1=pd.read_excel(r'D:/python_project/projects/region_adverting/city_consume.xlsx')
+
+#当日投放
+throwin_yesterday=df1['广告总额'].sum()
+#当日总进线
+inline_yesterday=df1['进线人数'].sum()
+#单粉成本
+single_cost=throwin_yesterday/inline_yesterday
+
+df2=pd.read_excel(r'D:/python_project/projects/national_sales_code/全国市场门店销售日报表临时用.xlsx')
+
+list_ccc=['平台','广东江苏河南区域','武汉-北京','浙江区域','西南区域']
+
+df2=df2.loc[df2['大区'].isin(list_ccc),:]
+#复购金额
+repuchase_amount=df2['复购成交金额'].sum()
+
+#商品单业绩 
+product_order_amount=df2['商品订单金额'].sum()
+#服务单业绩
+serve_order_amount=df2['服务订单金额'].sum()
+#充值金额
+recharge_amount=df2['充值金额'].sum()
+#退款总额
+refund_amount=df2['总退款金额'].sum()
+
+#当日合计销售金额
+actual_transaction_amount=df2['实际成交金额'].sum()
+#投产比
+input_output_ratio=actual_transaction_amount/throwin_yesterday
+
+#线下成交人数
+upgrade_people=df2['升单人数'].sum()
+repuchase_people=df2['复购成交人数'].sum()
+offline_deal_people=upgrade_people+repuchase_people
+
+#退卡数
+card_return_num=df2['体验卡退款数量'].sum()
+#退卡金额
+card_return_amount=df2['体验卡退款金额'].sum()
+
+df3=pd.read_excel('礼宾客服.xlsx')
+
+df3=df3.loc[df3['实付金额']>0,:]
+df5=df3.copy()
+
+df3.astype({'订单创建日期':'str'})
+df3=df3.loc[df3['订单创建日期'].str.contains(date),:]
+
+#千元以上卡项数量
+over_thousand_num=df3.loc[(df3['实付金额']>=999)&(df3['套餐分类']=='礼宾售卡')&(df3['是否退款']=='否'),'订单编号'].count()
+#千元以下卡项数量
+loewr_thousand_num=df3.loc[(df3['实付金额']<999)&(df3['套餐分类']=='礼宾售卡')&(df3['是否退款']=='否'),'订单编号'].count()
+#售卡数量合计
+card_total=df3.loc[(df3['套餐分类']=='礼宾售卡')&(df3['是否退款']=='否'),'订单编号'].count()
+#千元以上卡占比
+over_thousand_percent=over_thousand_num/card_total
+
+#升级卡金额
+upgrade_card_amount=df3.loc[df3['套餐分类']=='礼宾售卡升单','实付金额'].sum()
+##线上售卡金额合计
+card_amount=df3.loc[df3['是否退款']=='否','实付金额'].sum()-upgrade_card_amount
+#线下成交金额合计
+offline_amount_total=actual_transaction_amount-card_amount+refund_amount
+#线下成交客单价
+offline_per_sales=(offline_amount_total-upgrade_card_amount)/offline_deal_people
+#退款金额
+return_amount=refund_amount-card_return_amount
+
+
+#升单金额
+upgrade_order_amount=offline_amount_total-upgrade_card_amount-repuchase_amount
+
+
+#-----------销售月报计算-----------
+
+df4=pd.read_excel(r'D:/python_project/projects/national_sales_code/全国市场门店销售月报表临时用.xlsx')
+
+df4=df4.loc[df4['大区'].isin(list_ccc),:]
+
+#复购金额
+repuchase_amounts=df4['复购成交金额'].sum()
+
+#商品单业绩 
+product_order_amounts=df4['商品订单金额'].sum()
+#服务单业绩
+serve_order_amounts=df4['服务订单金额'].sum()
+#充值金额
+recharge_amounts=df4['充值金额'].sum()
+#退款总额
+refund_amounts=df4['总退款金额'].sum()
+
+#当日合计销售金额
+actual_transaction_amounts=df4['实际成交金额'].sum()
+
+#线下成交人数
+upgrade_people_all=df4['升单人数'].sum()
+repuchase_people_all=df4['复购成交人数'].sum()
+offline_deal_number=upgrade_people_all+repuchase_people_all
+
+#退卡数
+card_return_number=df4['体验卡退款数量'].sum()
+#退卡金额
+card_return_amounts=df4['体验卡退款金额'].sum()
+
+df5.astype({'订单创建日期':'str'})
+df5=df5.loc[~(df5['订单创建日期'].str.contains(date1)),:]
+
+#千元以上卡项数量
+over_thousand_number=df5.loc[(df5['实付金额']>=999)&(df5['套餐分类']=='礼宾售卡')&(df5['是否退款']=='否'),'订单编号'].count()
+#千元以下卡项数量
+loewr_thousand_number=df5.loc[(df5['实付金额']<999)&(df5['套餐分类']=='礼宾售卡')&(df5['是否退款']=='否'),'订单编号'].count()
+#售卡数量合计
+card_totals=df5.loc[(df5['套餐分类']=='礼宾售卡')&(df5['是否退款']=='否'),'订单编号'].count()
+#千元以上卡占比
+over_thousand_percents=over_thousand_number/card_totals
+
+#升级卡金额
+upgrade_card_amounts=df5.loc[df5['套餐分类']=='礼宾售卡升单','实付金额'].sum()
+#线上售卡金额合计
+card_amounts=df5.loc[df5['是否退款']=='否','实付金额'].sum()-upgrade_card_amounts
+#线下成交金额合计
+offline_amount_totals=actual_transaction_amounts-card_amounts+refund_amounts
+#线下成交客单价
+offline_per_sale=(offline_amount_totals-upgrade_card_amounts)/offline_deal_number
+#退款金额
+return_amounts=refund_amounts-card_return_amounts
+
+
+#升单金额
+upgrade_order_amounts=offline_amount_totals-repuchase_amounts-upgrade_card_amounts
+
+
+df6=pd.read_excel(r'D:/python_project/projects/national_sales_code/各城市广告费累计.xlsx')
+
+
+#当月投放
+put_in_month=df6['广告总额'].sum()
+
+#当月总进线
+total_inline_month=df6['进线人数'].sum()
+
+
+
+#单粉成本
+single_cost_month=put_in_month/total_inline_month
+
+
+#整体投产比
+in_out_ratio=actual_transaction_amounts/put_in_month
+
+
+
+from openpyxl import load_workbook
+wb = load_workbook(filename="销售汇总表模板.xlsx")
+ws = wb.active
+
+
+
+#日表写入
+
+
+
+ws.cell(row=3, column=1, value=throwin_yesterday)#当日投放
+ws.cell(row=3, column=2, value=inline_yesterday)#当日总进线
+ws.cell(row=3, column=5, value=single_cost)#单粉成本
+ws.cell(row=3, column=9, value=card_amount)#线上售卡金额合计
+ws.cell(row=5, column=7, value=upgrade_order_amount)#升单金额
+ws.cell(row=5, column=8, value=repuchase_amount)#复购金额
+ws.cell(row=7, column=6, value=product_order_amount)#商品单业绩 
+ws.cell(row=7, column=8, value=serve_order_amount)#服务单业绩
+ws.cell(row=7, column=10, value=recharge_amount)#充值金额
+ws.cell(row=7, column=12, value=refund_amount)#退款总额
+ws.cell(row=8, column=9, value=actual_transaction_amount)#当日合计销售金额
+ws.cell(row=8, column=11, value=input_output_ratio)#投产比
+ws.cell(row=5, column=9, value=offline_deal_people)#线下成交人数
+
+
+
+ws.cell(row=3, column=6, value=over_thousand_num)#千元以上卡项数量
+ws.cell(row=3, column=7, value=loewr_thousand_num)#千元以下卡项数量
+ws.cell(row=3, column=8, value=card_total)#售卡数量合计
+ws.cell(row=3, column=10, value=over_thousand_percent)#千元以上卡占比
+ws.cell(row=3, column=11, value=card_return_num)#退卡数
+ws.cell(row=3, column=12, value=card_return_amount)#退卡金额
+ws.cell(row=5, column=6, value=upgrade_card_amount)#升级卡金额
+ws.cell(row=5, column=10, value=offline_amount_total)#线下成交金额合计
+ws.cell(row=5, column=11, value=offline_per_sales)#线下成交客单价
+ws.cell(row=5, column=12, value=return_amount)#退款金额
+
+
+#月表写入
+
+
+ws.cell(row=3, column=21, value=card_amounts)#线上售卡金额合计
+ws.cell(row=5, column=19, value=upgrade_order_amounts)#升单金额
+ws.cell(row=5, column=20, value=repuchase_amounts)#复购金额
+ws.cell(row=7, column=18, value=product_order_amounts)#商品单业绩 
+ws.cell(row=7, column=20, value=serve_order_amounts)#服务单业绩
+ws.cell(row=7, column=22, value=recharge_amounts)#充值金额
+ws.cell(row=7, column=24, value=refund_amounts)#退款总额
+ws.cell(row=8, column=21, value=actual_transaction_amounts)#当日合计销售金额
+ws.cell(row=5, column=21, value=offline_deal_number)#线下成交人数
+
+
+ws.cell(row=3, column=18, value=over_thousand_number)#千元以上卡项数量
+ws.cell(row=3, column=19, value=loewr_thousand_number)#千元以下卡项数量
+ws.cell(row=3, column=20, value=card_totals)#售卡数量合计
+ws.cell(row=3, column=22, value=over_thousand_percents)#千元以上卡占比
+ws.cell(row=3, column=23, value=card_return_number)#退卡数
+ws.cell(row=3, column=24, value=card_return_amounts)#退卡金额
+ws.cell(row=5, column=18, value=upgrade_card_amounts)#升级卡金额
+ws.cell(row=5, column=22, value=offline_amount_totals)#线下成交金额合计
+ws.cell(row=5, column=23, value=offline_per_sale)#线下成交客单价
+ws.cell(row=5, column=24, value=return_amounts)#退款金额
+ws.cell(row=3, column=13, value=put_in_month)#当月投放
+ws.cell(row=3, column=14, value=total_inline_month)#当月总进线
+ws.cell(row=3, column=17, value=single_cost_month)#单粉成本
+ws.cell(row=8, column=23, value=in_out_ratio)#整体投产比
+
+ws.cell(1, 1).value = f'医普茂销售日报（0点-0点）{date}'
+
+wb.save(filename = f'医普茂当日销售汇总表{date}.xlsx')
+
+excel2img.export_img(f'医普茂当日销售汇总表{date}.xlsx', '医普茂当日销售汇总表'+date+'.png', "Sheet1", None)
+
+def wx_image(image):
+    with open(image, 'rb') as file:  # 转换图片成base64格式
+        data = file.read()
+        encodestr = base64.b64encode(data)
+        image_data = str(encodestr, 'utf-8')
+
+    with open(image, 'rb') as file:  # 图片的MD5值
+        md = hashlib.md5()
+        md.update(file.read())
+        image_md5 = md.hexdigest()
+
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b3ab38ac-adbc-489d-adc1-dadd61c2f98c"  # 填上机器人Webhook地址
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "msgtype": "image",
+        "image": {
+            "base64": image_data,
+            "md5": image_md5
+        }
+    }
+    result = requests.post(url, headers=headers, json=data)
+    return result
+
+wx_image(f'医普茂当日销售汇总表{date}.png')  # 传入图片路径
+
+def upload_file(file_path, wx_upload_url):
+    file_name = file_path.split("/")[-1]
+    with open(file_path, 'rb') as f:
+        length = os.path.getsize(file_path)
+        data = f.read()
+    headers = {"Content-Type": "application/octet-stream"}
+    params = {
+        "filename": file_name,
+        "filelength": length,
+    }
+    file_data = copy(params)
+    file_data['file'] = (file_path.split('/')[-1:][0], data)
+    encode_data = encode_multipart_formdata(file_data)
+    file_data = encode_data[0]
+    headers['Content-Type'] = encode_data[1]
+    r = requests.post(wx_upload_url, data=file_data, headers=headers)
+    media_id = r.json()['media_id']
+    return media_id
+
+def qi_ye_wei_xin_file(wx_url, media_id):
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msgtype": "file",
+        "file": {
+            "media_id": media_id
+        }
+    }
+    r = requests.post(
+        url=wx_url,
+        headers=headers, json=data)
+
+test_report = f'医普茂当日销售汇总表{date}.xlsx'
+
+wx_api_key = 'b3ab38ac-adbc-489d-adc1-dadd61c2f98c'
+wx_upload_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/upload_media?key={}&type=file".format(wx_api_key)
+wx_url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}'.format(wx_api_key)
+media_id = upload_file(test_report, wx_upload_url)
+qi_ye_wei_xin_file(wx_url, media_id)
+
+os.remove(f'医普茂当日销售汇总表{date}.png')
+os.remove(f'医普茂当日销售汇总表{date}.xlsx')
